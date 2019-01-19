4c3e5aa7f563958f9b75eaabcc6c8d4cf11476d3
endurance21 <43696525+endurance21@users.noreply.github.com>
2019-01-19 11:44:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -149,38 +149,30 @@ function setupWebAudio() {
 
  
 function resolveCollision(particle, otherParticle) {
-    const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
-    const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
-
-    const xDist = otherParticle.x - particle.x;
-    const yDist = otherParticle.y - particle.y;
-
-     
-    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-
-       
-        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+    
+    
+      const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
  
         const m1 = particle.mass;
         const m2 = otherParticle.mass;
 
         
         const u1 = rotate(particle.velocity, angle);
-        const u2 = rotate(otherParticle.velocity, angle);
+         const u2 = rotate(otherParticle.velocity, angle);
 
       
-        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };                     /////////////////UTILITY COLLISON FUNCTION ////ULTIMATE 
+        const v1 = { x: u2.x , y: u1.y };                     /////////////////UTILITY COLLISON FUNCTION ////ULTIMATE 
 
-        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+        const v2 = { x:  u1.x , y: u2.y };
 
        
         const vFinal1 = rotate(v1, -angle);
-        const vFinal2 = rotate(v2, -angle);
+         const vFinal2 = rotate(v2, -angle);
  
         particle.velocity.x = vFinal1.x;
-        particle.velocity.y = vFinal1.y;
+         particle.velocity.y = vFinal1.y;
 
-        otherParticle.velocity.x = vFinal2.x;
+          otherParticle.velocity.x = vFinal2.x;
         otherParticle.velocity.y = vFinal2.y;
     }
 }
